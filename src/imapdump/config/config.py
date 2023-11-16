d9874e7c62529dfb376eee11814e5280af50b0ea
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2023-11-16 15:57:52 +0100

    add root config class

---
+++ src/imapdump/config/config.py
@@ -0,0 +1,9 @@
+from typing import Dict
+from dataclasses import dataclass, field
+
+from ..config.imap_config import ImapConfig
+
+@dataclass
+class Config:    
+    servers: Dict[str, ImapConfig] = field(default_factory=dict)
+    dump_folder: str = "dump"
