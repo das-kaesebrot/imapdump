fde640eb4a5960428e89afaeca53bda919b6f814
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2023-11-17 02:59:36 +0100

    format with black, add comments, compare hashes to skip write of existing files

--- src/imapdump/imap/dumper.py
+++ src/imapdump/imap/dumper.py
@@ -1,83 +1,137 @@
 import logging
 import os
-import shutil
 from ..utils.str_utils import envelope_to_msg_title
+from ..utils.hash_utils import filehash, bytehash
 from imapclient.response_types import Envelope
 from ..config.imap_config import ImapConfig
 from imapclient import IMAPClient
 
+
 class ImapDumper:
     _client: IMAPClient
     _folder: str = None
-    
     _logger: logging.Logger
-    
+
     def __init__(self, config: ImapConfig, name: str, dump_folder: str) -> None:
-                
         self._client = IMAPClient(
-            host=config.host,
-            port=config.port,
-            use_uid=True,
-            ssl=config.ssl)
-        
+            host=config.host, port=config.port, use_uid=True, ssl=config.ssl
+        )
+
         self._folder = os.path.join(dump_folder, name)
-        
+
         self._logger = logging.getLogger(f"dumper.{name}")
-        
+
         self._client.login(config.username, config.password)
         self._set_idle(True)
-            
+
     def dump(self):
+        messages_per_folder = self._get_all_messages()
+        self._write_message_files(messages_per_folder)
+
+    def _get_all_messages(self) -> dict:
+        # stop idling
         self._set_idle(False)
-        
+
         messages_in_account = {}
-        
+
+        # iterate over all folders in IMAP account
         for flags, delim, name in self._client.list_folders():
             self._logger.debug(f"{flags=}, {delim=}, {name=}")
-            
+
+            # select folder to be examined
             self._client.select_folder(name, readonly=True)
-            
+
             msg_ids = self._client.search()
-            
+
             if len(msg_ids) <= 0:
                 continue
-            
+
             messages_in_directory = {}
-            
-            for msgid, data in self._client.fetch(messages=msg_ids, data=['RFC822', 'ENVELOPE']).items():
-                envelope = data.get(b'ENVELOPE')
-                rfc822 = data.get(b'RFC822')
-                
+
+            # Get envelope info and entire message (RFC822)
+            for msgid, data in self._client.fetch(
+                messages=msg_ids, data=["RFC822", "ENVELOPE"]
+            ).items():
+                envelope = data.get(b"ENVELOPE")
+                rfc822 = data.get(b"RFC822")
+
+                # sanity check
                 assert isinstance(envelope, Envelope)
-                
-                msg_title = envelope_to_msg_title(envelope)
-                                
-                self._logger.info(msg_title)
-                self._logger.debug(msgid)
-                
-                messages_in_directory[msg_title] = rfc822
-                
+
+                msg_filename = f"{envelope_to_msg_title(envelope)}.eml"
+
+                # add message to folder specific dict using generated title as key
+                messages_in_directory[msg_filename] = rfc822
+
+            self._logger.info(
+                f"Found {len(messages_in_directory.keys())} messages in directory '{name}'"
+            )
+
+            # add message title/message content dict as directory dict entry
             messages_in_account[name] = messages_in_directory
-            
+
+        # back to idling
         self._set_idle(True)
-        
-        if os.path.exists(self._folder):
-            shutil.rmtree(self._folder)
-        
-        for subfolder, messages in messages_in_account.items():
+
+        return messages_in_account
+
+    def _write_message_files(self, messages: dict):
+        num_new = 0
+        num_skipped = 0
+        num_all = 0
+
+        for subfolder, messages in messages.items():
             assert isinstance(messages, dict)
-            
+
             account_subfolder_write_path = os.path.join(self._folder, subfolder)
-            os.makedirs(account_subfolder_write_path)
-            
+
+            os.makedirs(account_subfolder_write_path, exist_ok=True)
+
+            num_all += len(messages)
+
             for message_filename, message_content in messages.items():
-                filename = os.path.join(account_subfolder_write_path, f"{message_filename}.eml")
-                
-                with open(filename, 'wb') as f:
+                filename = os.path.join(account_subfolder_write_path, message_filename)
+
+                # check if message was already saved (by md5 hash comparison) and skip if that's the case
+                if os.path.isfile(filename):
+                    message_hash = bytehash(message_content)
+                    if filehash(filename) == message_hash:
+                        self._logger.debug(
+                            f"Skipping file '{filename}': Already exists and hash '{message_hash}' matches"
+                        )
+                        num_skipped += 1
+                        continue
+
+                self._logger.debug(f"Writing new file '{filename}'")
+                num_new += 1
+                with open(filename, "wb") as f:
                     f.write(message_content)
-            
-            
+
+            self._cleanup_leftovers(messages.keys(), account_subfolder_write_path)
+
+        self._logger.info(
+            f"Done writing! New: {num_new}, skipped: {num_skipped}, total: {num_all}"
+        )
+
+    # cleans up all unexpected files in a subfolder
+    def _cleanup_leftovers(self, allowed_files: list[str], folder: str):
+        files_in_directory = os.listdir(folder)
+        leftovers = [
+            filename for filename in files_in_directory if filename not in allowed_files
+        ]
+
+        self._logger.debug(f"Found {len(leftovers)} items not in backup")
+
+        for filename in leftovers:
+            filename = os.path.join(folder, filename)
+            if not os.path.isfile(filename):
+                continue
+
+            self._logger.debug(f"Removing '{filename}'")
+            os.remove(filename)
+
     def _set_idle(self, idle: bool):
-        if idle: self._client.idle()
-        else: self._client.idle_done()
-        
+        if idle:
+            self._client.idle()
+        else:
+            self._client.idle_done()
