bd1d9b468a3166de35470767ac7b55fb7951a75f
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2023-11-17 02:54:42 +0100

    add hash utils

---
+++ src/imapdump/utils/hash_utils.py
@@ -0,0 +1,8 @@
+import hashlib
+
+def filehash(filename: str) -> str:
+    with open(filename, 'rb', buffering=0) as f:
+        return hashlib.file_digest(f, 'md5').hexdigest()
+    
+def bytehash(byteobject) -> str:
+    return hashlib.md5(byteobject).hexdigest()
