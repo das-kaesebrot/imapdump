f376061d5290452e7ef21117040d090fa1304cd0
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2023-11-18 01:39:55 +0100

    fix: limit message titles

--- src/imapdump/utils/str_utils.py
+++ src/imapdump/utils/str_utils.py
@@ -1,9 +1,30 @@
 from imapclient.response_types import Envelope, Address
-from urllib.parse import quote
+
+_max_length = 255 - len(".eml")
 
 
 def envelope_to_msg_title(envelope: Envelope) -> str:
-    return f"{envelope.message_id.decode().strip('<').strip('>')} - {addr_to_str(envelope.sender)} - {envelope.date.isoformat()}"
+    ret_str_arr = []
+
+    if envelope.subject:
+        ret_str_arr.append(envelope.subject.decode())
+
+    ret_str_arr.append(
+        addr_to_str(envelope.sender if envelope.sender else envelope.from_)
+    )
+
+    if envelope.date:
+        ret_str_arr.append(str(int(envelope.date.timestamp())))
+
+    ret_str = " - ".join(ret_str_arr).replace("/", "").replace("\\", "")
+
+    if not len(ret_str) > _max_length:
+        return ret_str
+
+    cutoff_length = len(ret_str) - _max_length
+    ret_str_arr[0] = ret_str_arr[0][:-cutoff_length]
+
+    return " - ".join(ret_str_arr).replace("/", "").replace("\\", "")
 
 
 def addr_to_str(addresses: tuple[Address]) -> str:
