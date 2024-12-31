4da410bb424fb5bee46dea7a985b2d53116cd63d
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2024-12-31 12:58:21 +0100

    parse message id as string

--- src/imapclient.rs
+++ src/imapclient.rs
@@ -6,6 +6,8 @@ use sha2::{Digest, Sha256};
 extern crate imap;
 extern crate native_tls;
 
+// https://www.atmail.com/blog/imap-commands/
+
 pub async fn get_client(
     host: String,
     port: u16,
@@ -32,8 +34,8 @@ pub async fn logout_client(
 
 pub fn get_message_hashes(
     session: &mut imap::Session<native_tls::TlsStream<std::net::TcpStream>>,
-) -> Option<HashMap<Vec<u8>, Vec<u8>>> {
-    let mut hash_per_message_id: HashMap<Vec<u8>, Vec<u8>> = HashMap::new();
+) -> Option<HashMap<String, Vec<u8>>> {
+    let mut hash_per_message_id: HashMap<String, Vec<u8>> = HashMap::new();
 
     // Fetch all folders
     match session.list(Some(""), Some("*")) {
@@ -74,7 +76,7 @@ pub fn get_message_hashes(
                             .map(|f| f.to_string())
                             .collect::<Vec<String>>()
                             .join(","),
-                        "FAST",
+                        "ENVELOPE",
                     )
                     .unwrap();
 
@@ -90,16 +92,16 @@ pub fn get_message_hashes(
                     let message = result.unwrap();
 
                     let date = message.date.unwrap();
-                    let message_id = message.message_id.unwrap();
+                    let message_id = String::from_utf8(message.message_id.unwrap_or_default().to_vec()).unwrap();
 
-                    let hasher = Sha256::new();
+                    let hasher =    Sha256::new();
                     let generated_hash = hasher
                         .chain_update(date)
-                        .chain_update(message_id)
+                        .chain_update(message_id.clone())
                         .finalize()
                         .to_vec();
 
-                    hash_per_message_id.insert(message_id.to_vec(), generated_hash);
+                    hash_per_message_id.insert(message_id, generated_hash);
                 }
             }
         }
