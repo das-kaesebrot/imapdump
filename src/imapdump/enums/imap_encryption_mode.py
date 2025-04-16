d9757eddf14ef88c96fc23a5f6cc2d89c9eaff52
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2025-04-16 11:54:57 +0200

    add ssl encryption mode enum

---
+++ src/imapdump/enums/imap_encryption_mode.py
@@ -0,0 +1,11 @@
+from enum import StrEnum, auto
+
+
+class ImapEncryptionMode(StrEnum):
+    NONE = auto()
+    SSL = auto()
+    STARTTLS = auto()
+    
+    @staticmethod
+    def list():
+        return list(map(lambda c: c.value, ImapEncryptionMode))
