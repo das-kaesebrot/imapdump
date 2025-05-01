41da04a13408b57dc2436a9cff29897ae778eb13
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2025-05-01 12:11:55 +0200

    refactor config to allow passing args to dataclass directly

--- src/imapdump/config/imap_config.py
+++ src/imapdump/config/imap_config.py
@@ -1,11 +1,16 @@
+from ..enums.imap_encryption_mode import ImapEncryptionMode
 from dataclasses import dataclass, field
 
 
 @dataclass
 class ImapDumpConfig:
+    loglevel: str
     host: str
+    port: int = 993
     username: str = None
     password: str = None
-    port: int = 993
-    ssl: bool = True
-    ignored: list[str] = field(default_factory=list)
+    database_file: str = None
+    encryption_mode: ImapEncryptionMode = ImapEncryptionMode.SSL
+    folder_regex: str = "^.*$"
+    config: dict = None
+    
