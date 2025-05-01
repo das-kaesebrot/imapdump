e901ab1e5b884ec3148201b6072411b1073b501e
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2025-05-01 13:11:46 +0200

    add static method for generating id to mail entity

--- src/imapdump/models/mail.py
+++ src/imapdump/models/mail.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
 from sqlalchemy.sql import func
 from .base import Base
+from hashlib import sha256
 
 
 class Mail(Base):
@@ -15,3 +16,8 @@ class Mail(Base):
     size: Mapped[int] = mapped_column()
     modified: Mapped[datetime] = mapped_column(DateTime, onupdate=func.now(), default=func.now())
     created: Mapped[datetime] = mapped_column(DateTime, default=func.now())
+    
+    @staticmethod
+    def generate_id(folder_name: str, message_id: str) -> str:
+        return sha256(f"{folder_name}_{message_id}".encode()).hexdigest()
+    
