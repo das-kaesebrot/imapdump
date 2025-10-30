9a8450d1509cadc34ddc633bddd2a2e8fca6deee
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2025-10-30 23:14:33 +0100

    feature: add dataclass for reading values from config file

---
+++ imapdump/config/fileconfig.py
@@ -0,0 +1,16 @@
+from ..enums.imap_encryption_mode import ImapEncryptionMode
+from .default_values import ImapDumpConfigDefaults
+from dataclasses import dataclass
+
+
+@dataclass
+class ImapDumpFileConfig:
+    host: str
+    port: int = ImapDumpConfigDefaults.PORT
+    username: str = ImapDumpConfigDefaults.USERNAME
+    password: str = ImapDumpConfigDefaults.PASSWORD
+    database_file: str = ImapDumpConfigDefaults.DATABASE_FILE
+    encryption_mode: ImapEncryptionMode = ImapDumpConfigDefaults.ENCRYPTION_MODE
+    folder_regex: str = ImapDumpConfigDefaults.FOLDER_REGEX
+    dump_folder: str = ImapDumpConfigDefaults.DUMP_FOLDER
+    
